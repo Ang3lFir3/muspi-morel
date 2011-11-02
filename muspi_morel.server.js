e12d61645be9c2df1f429aaa7a640825a14975d9
Ang3lFir3 <ang3lfir3@gmail.com>
2011-11-01 21:39:07 -0700

    making some changes for heroku

--- muspi_morel.server.js
+++ muspi_morel.server.js
@@ -3,6 +3,10 @@ var url = require('url');
 
 var core_text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum".split(' ');
 
+
+var port = process.env.PORT || 1337;
+console.log("ur port is " + port);
+
 http.createServer(function (req, res) {
 	var question = url.parse(req.url, true);
 	var query_size = question.query.size;
@@ -24,6 +28,6 @@ http.createServer(function (req, res) {
 	
 	res.end("why you gotta break stuff?\n");
 	
-}).listen(1337, "127.0.0.1");
+}).listen(port);
 
 console.log("OKay I hooked you up with a server.... don't break anything");
