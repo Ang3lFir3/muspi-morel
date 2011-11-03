e81adedc95041bb6c7388f64b0c7bbf33b527905
Ang3lFir3 <ang3lfir3@gmail.com>
2011-11-02 22:58:44 -0700

    decided to use the express module ... much better

--- muspi_morel.server.js
+++ muspi_morel.server.js
@@ -1,5 +1,4 @@
-var http = require('http');
-var url = require('url');
+var express = require('express');
 
 var core_text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum".split(' ');
 
@@ -7,19 +6,23 @@ var core_text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed d
 var port = process.env.PORT || 1337;
 console.log("ur port is " + port);
 
-http.createServer(function (req, res) {
-	res.writeHead(200, {'Content-Type': 'text/plain'});
+var app = express.createServer(express.logger());
 
-	var question = url.parse(req.url, true);
-	if(question.pathname == "/" && question.query.size == undefined){
-		res.end(" use a query string with the size value set to some number. ie: /?size=13 \n");
+app.get('/', function(req, res){
+	res.send("Usage ie: /size/13 \n", { 'Content-Type': 'text/plain' }, 200)
+});
+
+app.get('/size/:size', function(req, res){
+	var query_size = parseInt(req.params.size);
+	console.log("I found a query for size : " + query_size + " of type" + typeof(query_size));
+
+	if(isNaN(query_size)){
+		res.send("huh? why you gotta break stuff?\n make sure you are using a number, Jeff\n", 400);
 	}
-
-	var query_size = question.query.size;
 
 	if(query_size > 0 && query_size <= core_text.length ){
 		var text = core_text.slice(0, query_size);
-		res.end(text.join(' ') + "\n");
+		res.send(text.join(' ') + "\n", 200);
 	}
 	if(query_size > core_text.length ){
 		while(core_text.length < query_size){
@@ -28,11 +31,11 @@ http.createServer(function (req, res) {
 		}
 		
 		var text = core_text.slice(0, query_size)
-		res.end(text.join(' ') + "\n");
-	}
-	
-	res.end("why you gotta break stuff?\n");
-	
-}).listen(port);
+		res.send(text.join(' ') + "\n", 200);
+	}	
+});
 
-console.log("OKay I hooked you up with a server.... don't break anything");
+
+app.listen(port, function(){
+	console.log("OKay I hooked you up with a server.... don't break anything");
+});
