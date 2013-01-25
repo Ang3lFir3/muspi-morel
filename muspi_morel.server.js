080fb236ac59127eb084701d69d0e94f6fc75f6a
Ang3lfir3 <ang3lfir3@gmail.com>
2013-01-24 17:38:54 -0800

    add some error handling for situations where people ask for stupid things

--- muspi_morel.server.js
+++ muspi_morel.server.js
@@ -1,5 +1,9 @@
 var express = require('express');
-var moment = require('moment');
+try{
+  var moment = require('moment');
+}
+catch(e){
+}
 
 var core_text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum".split(' ');
 
@@ -20,7 +24,7 @@ var shuffle = function(words){
 	}
 }
 
-var app = express.createServer(express.logger());
+var app = express(express.logger());
 
 app.get('/', function(req, res){
 	res.send("Usage ie: /size/13 \n /paragraph/2 \n /date/yyyy-mm-dd", { 'Content-Type': 'text/plain' }, 200)
@@ -30,23 +34,33 @@ app.get('/size/:size', function(req, res){
 	var query_size = parseInt(req.params.size);
 
 	if(isNaN(query_size)){
-		res.send("huh? why you gotta break stuff?\n make sure you are using a number.", 400);
+		res.send("huh? why you gotta break stuff?\n make sure you are using a number.\n", 400);
 	}
 
-	shuffle(core_text);
+  if(query_size > 9999){
+    res.send("huh? are you insane...\n look you do not really want that many words I promise.\n", 400)
+  }
+
+ 	shuffle(core_text);
 
 	if(query_size > 0 && query_size <= core_text.length ){
 		var text = core_text.slice(0, query_size);
 		res.send(text.join(' ') , 200);
+    text = null;
 	}
-	if(query_size > core_text.length ){
-		while(core_text.length < query_size){
+	
+  if(query_size > core_text.length ){
+		var originalSize = core_text.length;
+    while(core_text.length < query_size){
 			core_text = core_text.concat(core_text);
 			console.log("new length = " + core_text.length);
 		}
-		
+	
 		var text = core_text.slice(0, query_size)
 		res.send(text.join(' ') , 200);
+    
+    core_text = core_text.slice(0, originalSize);
+    text = null;
 	}	
 });
 
@@ -54,8 +68,12 @@ app.get('/paragraph/:count', function (request, response){
 	paragraph_count = request.params.count;
 
 	if(isNaN(paragraph_count)){
-		res.send("huh? why you gotta break stuff?\n make sure you are using a number, Jeff", 400);
+		response.send("huh? why you gotta break stuff?\n make sure you are using a number, Jeff\n", 400);
 	}
+ 
+  if(paragraph_count > 30){
+    response.send("huh? are you insane...\n look you do not really want that many paragraphs I promise.\n", 400)
+  }
 
 	var result = '';
 	for(var i = 0; i < paragraph_count; i++){
@@ -65,11 +83,6 @@ app.get('/paragraph/:count', function (request, response){
 
 	response.send(result, 200);
 });
-
-app.get('/shae', function (request, response))
-{
-  response.send("Hi shae", 200);
-}
 
 app.get('/date/:format', function (request, response){
 	var month = Math.random() *11; //months are zero based.... yeah i know i cryed too 
@@ -88,8 +101,14 @@ app.get('/date/:format', function (request, response){
 	}
 
 
-	var the_date = moment(new Date(year,month,day));
-	response.send(moment(the_date).format(request.params.format.toUpperCase()),	200);
+	console.log(moment);
+  if(moment){
+	  var the_date = moment(new Date(year,month,day));
+    response.send(moment(the_date).format(request.params.format.toUpperCase()),	200);
+  }
+  else{
+    response.send("01/01/0001", 200);
+  }
 });
 
 
